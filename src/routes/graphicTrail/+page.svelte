11bea9ddef54e8fbcceaa3bbbe6f702b77696ab2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2026-04-08 11:23:31 +0200

    trail editor done

--- src/routes/graphicTrail/+page.svelte
+++ src/routes/graphicTrail/+page.svelte
@@ -12,13 +12,19 @@
     import { getPath } from "./getPath.remote";
     import { error } from "@sveltejs/kit";
     import { iconmaker } from "./iconmaker";
-
     //leaflet map and the dom element
     let map: Map;
     let trail: Polyline[] = [];
     let trailMarkers: Marker[] = [];
     let makingTrail = $state(false);
     let loadingTrail = $state(0);
+    let showClickMenu = $state(false);
+    let clickMenuTarget = $state() as string;
+    let righclickTarget: number;
+    let menuPos = $state({ x: 0, y: 0 });
+    let insertingMarker = false;
+
+    let poiList = $state([]) as pointOfInterest[];
 
     class pointOfInterest {
         caption = $state("Namen Eintragen");
@@ -49,18 +55,13 @@
             if (serial) {
                 this.serial = serial;
             }
-            this.marker.on("click", () => heromaker(this));
         }
 
         destructor() {
             this.marker.remove();
             this.marker = null as any;
         }
-
-        //creates an html based icon with adjustable size and color
     }
-
-    let poiList = $state([]) as pointOfInterest[];
 
     function heromaker(poi: pointOfInterest) {
         poiList.forEach((element) => {
@@ -72,7 +73,25 @@
         poi.hero = true;
         poi.marker.setIcon(iconmaker("orange", 2));
     }
-
+    function onPageClick() {
+        showClickMenu = false;
+    }
+    function rightClickContextMenu(
+        e: LeafletMouseEvent,
+        target: "marker" | "polyline",
+    ) {
+        showClickMenu = true;
+        clickMenuTarget = target;
+        if (target == "marker") {
+            righclickTarget = trailMarkers.indexOf(e.target);
+        } else if (target == "polyline") {
+            righclickTarget = trail.indexOf(e.target);
+        }
+        menuPos = {
+            x: e.originalEvent.clientX,
+            y: e.originalEvent.clientY,
+        };
+    }
     // add the point at coordinats, with -1 as serial because its new and to the map
     function poiCreator(e: LeafletMouseEvent) {
         poiList.forEach((element) => {
@@ -85,9 +104,10 @@
         map.off("click", poiCreator);
         const listPosition = poiList.length - 1;
         poiList[listPosition].hero = true;
-        poiList[listPosition].marker
-            .setIcon(iconmaker("orange", 2))
-            .on("click", () => heromaker(poiList[listPosition]));
+        poiList[listPosition].marker.setIcon(iconmaker("orange", 2));
+        poiList[listPosition].marker.on("click", () =>
+            heromaker(poiList[listPosition]),
+        );
     }
 
     function responseToLatlngs(response: any) {
@@ -109,18 +129,18 @@
 
             trailMarkers = [new Marker(e.latlng, { draggable: true })];
 
-            trailMarkers[0].setIcon(iconmaker("green", 1)).addTo(map);
+            trailMarkers[0].setIcon(iconmaker("green", 1.2)).addTo(map);
         } else {
             // creating and saving positions in the array in case user clicks faster than the app responds
             trail.push(
                 new Polyline([
-                    [-1, -1],
-                    [0, 0],
-                ]),
+                    trailMarkers[trailMarkers.length - 1].getLatLng(),
+                    e.latlng,
+                ]).addTo(map),
             );
-            let trailPosition = trail.length - 1;
+            const trailPosition = trail.length - 1;
 
-            let coordinates = [
+            const coordinates = [
                 {
                     lat: trailMarkers[trailMarkers.length - 1].getLatLng()
                         .lat as number,
@@ -139,20 +159,16 @@
             }
             trailMarkers[trailMarkers.length - 1].dragging?.disable();
             //making markers as editing cornerstones for the trail
-            trailMarkers.push(new Marker(e.latlng, { draggable: false }));
-
-            let markerPosition = trailMarkers.length - 1;
-            trailMarkers[markerPosition].dragging?.enable();
+            trailMarkers.push(new Marker(e.latlng, { draggable: true }));
+            const markerPosition = trailMarkers.length - 1;
             trailMarkers[markerPosition]
                 .addTo(map)
-                .on("dragend", () =>
-                    moveTrail(
-                        trailMarkers.length - 2,
-                        trailMarkers.length - 1,
-                        trailMarkers.length,
-                    ),
-                )
+                .on("dragend", (e: any) => {
+                    const idx = trailMarkers.indexOf(e.target);
+                    moveTrail(idx - 1, idx, idx + 1);
+                })
                 .setIcon(iconmaker("orange", 1.5));
+
             try {
                 response = await getPath(coordinates);
             } catch {
@@ -164,9 +180,7 @@
                 e.latlng == trailMarkers[markerPosition].getLatLng()
             ) {
                 //making the new piece of trail
-                trail[trailPosition]
-                    .setLatLngs(responseToLatlngs(response))
-                    .addTo(map);
+                trail[trailPosition].setLatLngs(responseToLatlngs(response));
             } // else discarding the response
         }
         loadingTrail--;
@@ -179,15 +193,26 @@
             if (trailMarkers.length > 1) {
                 for (let i = 0; i < trailMarkers.length - 1; i++) {
                     trailMarkers[i]
-                        .on("dragend", () => moveTrail(i - 1, i, i + 1))
-                        .setIcon(iconmaker("blue", 1))
+                        .on("dragend", (e: any) => {
+                            const idx = trailMarkers.indexOf(e.target);
+                            moveTrail(idx - 1, idx, idx + 1);
+                        })
+                        .setIcon(iconmaker("blue", 1.2))
                         .dragging?.enable();
                 }
                 trailMarkers[trailMarkers.length - 1].setIcon(
-                    iconmaker("orange", 1),
+                    iconmaker("orange", 1.2),
                 );
-                trailMarkers[0].setIcon(iconmaker("green", 1));
+                trailMarkers[0].setIcon(iconmaker("green", 1.2));
             }
+            trailMarkers.forEach((m) =>
+                m.on("contextmenu", (e) => rightClickContextMenu(e, "marker")),
+            );
+            trail.forEach((p) =>
+                p.on("contextmenu", (e) =>
+                    rightClickContextMenu(e, "polyline"),
+                ),
+            );
         } else {
             map.on("click", trailMaker);
             makingTrail = true;
@@ -202,8 +227,10 @@
                 trailMarkers[trailMarkers.length - 1].setIcon(
                     iconmaker("orange", 1.5),
                 );
-                trailMarkers[0].setIcon(iconmaker("green", 1));
+                trailMarkers[0].setIcon(iconmaker("green", 1.2));
             }
+            trail.forEach((p) => p.off("contextmenu"));
+            trailMarkers.forEach((m) => m.off("contextmenu"));
         }
     }
 
@@ -226,6 +253,7 @@
                     lng: trailMarkers[next].getLatLng().lng as number,
                 },
             ];
+            trail[current].setLatLngs([curLatlng, nextLatlng]);
             //turn markers black and make them unmovable while processing
             try {
                 response = await getPath(coordinates);
@@ -260,14 +288,13 @@
                 },
             ];
             //save the location now incase the trail is build further
-
+            trail[previous].setLatLngs([preLatlng, curLatlng]);
             try {
                 response = await getPath(coordinates);
             } catch {
                 console.log(error);
             }
             //checking if markers have been moved while loading
-
             if (
                 trailMarkers.length > current &&
                 curLatlng == trailMarkers[current].getLatLng() &&
@@ -297,7 +324,8 @@
                 },
             ];
             //turn markers black and make them unmovable
-
+            trail[previous].setLatLngs([preLatlng, curLatlng]);
+            trail[current].setLatLngs([curLatlng, nextLatlng]);
             // since mapbox only resurns 1 array for any number of coordinates and there isn't a way in the api to mark where
             let part1;
             let part2;
@@ -336,17 +364,14 @@
             if (makingTrail) {
                 trailMarkers[trailMarkers.length - 1]
                     .setIcon(iconmaker("orange", 1.5))
-                    .on("dragend", () =>
-                        moveTrail(
-                            trailMarkers.length - 2,
-                            trailMarkers.length - 1,
-                            trailMarkers.length,
-                        ),
-                    )
+                    .on("dragend", (e: any) => {
+                        const idx = trailMarkers.indexOf(e.target);
+                        moveTrail(idx - 1, idx, idx + 1);
+                    })
                     .dragging?.enable();
             } else {
                 trailMarkers[trailMarkers.length - 1].setIcon(
-                    iconmaker("orange", 1),
+                    iconmaker("orange", 1.2),
                 );
             }
         } else if (trailMarkers.length == 1) {
@@ -358,6 +383,130 @@
             trail[trail.length - 1].removeFrom(map);
             trail.pop();
         }
+    }
+
+    async function deleteWaypoint(righclickTarget: number) {
+        loadingTrail++;
+
+        if (righclickTarget == trailMarkers.length - 1) {
+            deleteTrailend();
+        } else if (righclickTarget == 0) {
+            trailMarkers[righclickTarget].off("dragend").remove();
+            trail[righclickTarget].remove();
+            trailMarkers.shift();
+            trail.shift();
+            trailMarkers[0].setIcon(iconmaker("green", 1.2));
+        } else {
+            trailMarkers[righclickTarget].remove();
+            trail[righclickTarget].remove();
+            trailMarkers.splice(righclickTarget, 1);
+            trail.splice(righclickTarget, 1);
+
+            const startLatlng = trailMarkers[righclickTarget - 1].getLatLng();
+            const endLatlng = trailMarkers[righclickTarget].getLatLng();
+
+            trail[righclickTarget - 1].setLatLngs([startLatlng, endLatlng]);
+
+            const coordinates = [
+                {
+                    lat: startLatlng.lat as number,
+                    lng: startLatlng.lng as number,
+                },
+                {
+                    lat: endLatlng.lat as number,
+                    lng: endLatlng.lng as number,
+                },
+            ];
+
+            let response;
+
+            try {
+                response = await getPath(coordinates);
+            } catch {
+                console.log(error);
+            }
+
+            if (
+                startLatlng == trailMarkers[righclickTarget - 1].getLatLng() &&
+                endLatlng == trailMarkers[righclickTarget].getLatLng()
+            ) {
+                trail[righclickTarget - 1].setLatLngs(
+                    responseToLatlngs(response),
+                );
+            }
+        }
+        loadingTrail--;
+    }
+
+    function insertSwitch() {
+        insertMarker: true;
+        map.on("click", (e) => insertMarker(e, righclickTarget));
+    }
+
+    async function insertMarker(
+        event: LeafletMouseEvent,
+        righclickTarget: number,
+    ) {
+        insertingMarker = false;
+        loadingTrail++;
+        trailMarkers.splice(
+            righclickTarget + 1,
+            0,
+            new Marker(event.latlng, { draggable: true }),
+        );
+        trailMarkers[righclickTarget + 1]
+            .on("dragend", (e: any) => {
+                const idx = trailMarkers.indexOf(e.target);
+                moveTrail(idx - 1, idx, idx + 1);
+            })
+            .on("contextmenu", (e) => rightClickContextMenu(e, "marker"))
+            .setIcon(iconmaker("blue", 1))
+            .addTo(map);
+
+        const startLatlng = trailMarkers[righclickTarget].getLatLng();
+        const midLatlng = event.latlng;
+        const endLatlng = trailMarkers[righclickTarget + 2].getLatLng();
+
+        trail[righclickTarget].setLatLngs([startLatlng, midLatlng]);
+        trail.splice(
+            righclickTarget + 1,
+            0,
+            new Polyline([midLatlng, endLatlng]),
+        );
+        trail[righclickTarget + 1]
+            .on("contextmenu", (e) => rightClickContextMenu(e, "polyline"))
+            .addTo(map);
+        map.off("click");
+        const coordinates = [
+            { lat: startLatlng.lat as number, lng: startLatlng.lng as number },
+            { lat: midLatlng.lat as number, lng: midLatlng.lng as number },
+            { lat: endLatlng.lat as number, lng: endLatlng.lng as number },
+        ];
+
+        let part1;
+        let part2;
+        try {
+            [part1, part2] = await Promise.all([
+                getPath([coordinates[0], coordinates[1]]),
+                getPath([coordinates[1], coordinates[2]]),
+            ]);
+        } catch (err) {
+            console.log(err);
+        }
+
+        if (
+            startLatlng == trailMarkers[righclickTarget].getLatLng() &&
+            midLatlng == trailMarkers[righclickTarget + 1].getLatLng()
+        ) {
+            trail[righclickTarget].setLatLngs(responseToLatlngs(part1));
+        }
+        if (
+            endLatlng == trailMarkers[righclickTarget + 2].getLatLng() &&
+            midLatlng == trailMarkers[righclickTarget + 1].getLatLng()
+        ) {
+            trail[righclickTarget + 1].setLatLngs(responseToLatlngs(part2));
+        }
+        loadingTrail--;
     }
 
     //function to make the map once the html is created
@@ -385,8 +534,18 @@
     }
 </script>
 
+<svelte:window on:click={onPageClick} />
+
 <div class="alignment">
-    <div id="map" style="height:800px;width:1200px" use:createMap></div>
+    <div
+        id="map"
+        role="presentation"
+        style="height:800px;width:1200px"
+        use:createMap
+        oncontextmenu={(e) => {
+            e.preventDefault();
+        }}
+    ></div>
     <div class="block">
         {#if poiList}
             {#each poiList as poi}
@@ -422,6 +581,21 @@
         >
     </div>
 </div>
+{#if showClickMenu}
+    <div
+        style="position: absolute; top:{menuPos.y}px; left:{menuPos.x}px; z-index: 2000; background: white; border: 1px solid #ccc; padding: 4px; box-shadow: 2px 2px 5px rgba(0,0,0,0.2);"
+    >
+        {#if loadingTrail > 0}
+            <p>loading trail</p>
+        {:else if clickMenuTarget == "polyline"}
+            <button onclick={() => insertSwitch()}>add waypoint</button>
+        {:else if clickMenuTarget == "marker"}
+            <button onclick={() => deleteWaypoint(righclickTarget)}
+                >remove marker</button
+            >
+        {/if}
+    </div>
+{/if}
 
 <style>
     .alignment {
